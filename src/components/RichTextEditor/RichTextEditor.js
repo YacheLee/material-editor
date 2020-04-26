cf62935047f002585cbf87efc68a9cc8eaa61168
ya-che.li <ya-che.li@and.digital>
2020-04-26 10:17:56 +0100

    fix bug for padding

--- src/components/RichTextEditor/RichTextEditor.js
+++ src/components/RichTextEditor/RichTextEditor.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
 import EditorViewContext from "./contexts/EditorViewContext"
@@ -7,18 +8,22 @@ import Editor from './components/Editor';
 import Toolbar from './components/Toolbar';
 import normaliseValue from './normaliseValue';
 
+const PaperWrapper = styled(Paper)`
+  padding: 12px;
+`
+
 function RichTextEditor({defaultValue, onChange}) {
     const [editorView, setEditorView] = useState(null);
 
     return (
         <EditorViewContext.Provider value={{editorView, setEditorView}}>
-            <Paper elevation={3}>
+            <PaperWrapper elevation={3}>
                 {editorView && <Toolbar/>}
                 <Divider light={true} />
                 <div style={{margin: 12, overflow: 'auto'}}>
                     <Editor value={normaliseValue(defaultValue)} onChange={onChange}/>
                 </div>
-            </Paper>
+            </PaperWrapper>
         </EditorViewContext.Provider>
     );
 }
