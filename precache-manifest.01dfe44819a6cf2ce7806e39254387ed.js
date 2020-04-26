32996b84c35703e90becfb61158e6fa7918cd5f5
ya-che.li <ya-che.li@and.digital>
2020-04-26 15:42:01 +0100

    Updates

---
+++ precache-manifest.01dfe44819a6cf2ce7806e39254387ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c06c4377e84a8e8237364c6490e26c1",
+    "url": "/material-editor/index.html"
+  },
+  {
+    "revision": "b0b36c7796586c3f88a1",
+    "url": "/material-editor/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "f177ab60bf489934d50d",
+    "url": "/material-editor/static/js/2.b6503fdc.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/material-editor/static/js/2.b6503fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b36c7796586c3f88a1",
+    "url": "/material-editor/static/js/main.e9f1c911.chunk.js"
+  },
+  {
+    "revision": "2a92a0804937306bd8e3",
+    "url": "/material-editor/static/js/runtime-main.499bc23e.js"
+  }
+]);
