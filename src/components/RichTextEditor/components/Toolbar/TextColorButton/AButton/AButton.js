85d342e1e671ea8cc68a610b73027feb57fe7c40
ya-che.li <ya-che.li@and.digital>
2020-04-26 11:27:40 +0100

    refined AButton

--- src/components/RichTextEditor/components/Toolbar/TextColorButton/AButton/AButton.js
+++ src/components/RichTextEditor/components/Toolbar/TextColorButton/AButton/AButton.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import cn from "classnames";
-import {createUseStyles} from 'react-jss';
+import styled from 'styled-components';
 
-const useStyles = createUseStyles({
-    root: {
-        height: 28
-    }
-});
+const Root = styled.svg`
+  height: 28px;
+`;
 
 function AButton({color, onClick}) {
-    const classes = useStyles();
-
-    return <svg
+    return <Root
         onClick={onClick}
-        className={cn("MuiSvgIcon-root-77", classes.root)}
         focusable="false"
         viewBox="0 0 24 24"
         aria-hidden="true">
@@ -22,7 +16,7 @@ function AButton({color, onClick}) {
             <path fill={color} d="M0 20h24v4H0z"/>
             <path d="M11 3L5.5 17h2.25l1.12-3h6.25l1.12 3h2.25L13 3h-2zm-1.38 9L12 5.67 14.38 12H9.62z"/>
         </g>
-    </svg>
+    </Root>
 }
 
 AButton.defaultProps = {
